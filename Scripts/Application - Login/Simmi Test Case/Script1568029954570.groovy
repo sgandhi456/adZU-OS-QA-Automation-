f99bf139e21fc4c8541964903dcf33c441c98023
gansat <gansat@NOTEEG-12802.nexus.local>
2019-09-09 12:55:52 +0100

    Changes by simmi

--- Scripts/Application - Login/Simmi Test Case/Script1568029954570.groovy
+++ Scripts/Application - Login/Simmi Test Case/Script1568029954570.groovy
@@ -12,15 +12,4 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://bt.adzu4-staging.codeworldwide.com/#')
-
-WebUI.getUrl()
-
-WebUI.verifyMatch(new, 'https://bt.adzu4-staging.codeworldwide.com/#', false)
-
-WebUI.closeBrowser()
 
